eccd30866fc719e4507dfd74756829c1a6f4246a
Wael <wael.t.ivie@gmail.com>
2023-05-04 23:11:58 -0700

    update scripts, readme

--- scripts/markdown_to_csv.py
+++ scripts/markdown_to_csv.py
@@ -5,7 +5,6 @@ from io import StringIO
 
 
 # Read the Markdown file and extract the table as a string
-# path = os.path.dirname(os.path.abspath('sources_table'))
 with open('../sources_table/sources.md', 'r') as f:
     markdown_text = f.read()
 
@@ -15,9 +14,16 @@ table_text = table_match.group()
 # Convert the table string to a pandas DataFrame and then to CSV
 df = pd.read_csv(StringIO(table_text), sep='|', header=0, engine='python')
 
+new_df = df.loc[df[' Added to Index '] == ' ']
+new_df = pd.concat([new_df, df.loc[df[' Added to Index '] == ' No ']])
+new_df = pd.concat([new_df, df.loc[df[' Added to Index '] == 'No']])
+new_df = pd.concat([new_df, df.loc[df[' Added to Index '] == '  ']])
+
 # We need to drop the first and fifth column, which is empty and then remove the first row, which is the header
-df_clean = df.drop(df.columns[[0, 4]], axis=1)
-df_clean = df_clean.drop(df_clean.index[0])
+df_clean = new_df.drop(new_df.columns[[0, 5]], axis=1)
+
+# df_clean = df.drop(df.columns[[0, 4]], axis=1)
+# df_clean = df_clean.drop(df_clean.index[0])
 # remove the beginning and ending spaces from the column names
 df_clean.columns = df_clean.columns.str.strip()
 
@@ -27,15 +33,11 @@ df_clean['Url'] = df_clean['Url'].str.replace(r'\[.*\]', '', regex=True).str.rep
 df_clean['Type'] = df_clean['Type'].str.strip()
 df_clean['Description (optional)'] = df_clean['Description (optional)'].str.strip()
 
-csv_text = df_clean.to_csv(index=False)
 
-# Write the CSV to a file
-with open('../sources_table/sources.csv', 'w') as f:
-    f.write(csv_text)
-
+# Urls that are not in the index into a list
 url_list = df_clean.loc[df_clean['Type'] == 'Website']['Url'].tolist()
 
 
 # The save string to a file called url_sources.txt
-with open('../sources_table/url_sources.txt', 'w') as f:
+with open('../sources_table/new_url_sources.txt', 'w') as f:
     f.write(', '.join(url_list))
