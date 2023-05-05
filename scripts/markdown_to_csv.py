922624da0ad90ab14868b011123fc9daf06e3650
Wael <wael.t.ivie@gmail.com>
2023-05-04 21:42:51 -0700

    update script

--- scripts/markdown_to_csv.py
+++ scripts/markdown_to_csv.py
@@ -18,13 +18,24 @@ df = pd.read_csv(StringIO(table_text), sep='|', header=0, engine='python')
 # We need to drop the first and fifth column, which is empty and then remove the first row, which is the header
 df_clean = df.drop(df.columns[[0, 4]], axis=1)
 df_clean = df_clean.drop(df_clean.index[0])
+# remove the beginning and ending spaces from the column names
+df_clean.columns = df_clean.columns.str.strip()
 
-# almost there XD now we need to format the url column by removing everything inside the brackets
-df_clean[' Url '] = df_clean[' Url '].str.replace(r'\[.*\]', '', regex=True).str.replace(r'\(|\)', '', regex=True)
-
+# almost there XD now we need to format the url column by removing everything inside the brackets, remove the beginning and ending parentheses,
+# and remove the beginning and ending spaces
+df_clean['Url'] = df_clean['Url'].str.replace(r'\[.*\]', '', regex=True).str.replace(r'\(|\)', '', regex=True).str.strip()
+df_clean['Type'] = df_clean['Type'].str.strip()
+df_clean['Description (optional)'] = df_clean['Description (optional)'].str.strip()
 
 csv_text = df_clean.to_csv(index=False)
 
 # Write the CSV to a file
 with open('../sources_table/sources.csv', 'w') as f:
     f.write(csv_text)
+
+url_list = df_clean.loc[df_clean['Type'] == 'Website']['Url'].tolist()
+
+
+# The save string to a file called url_sources.txt
+with open('../sources_table/url_sources.txt', 'w') as f:
+    f.write(', '.join(url_list))
